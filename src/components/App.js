e95bfebb9fbcf0a471f9174dca4b8ab0976564d2
Vi Vo <aaronvo118@gmail.com>
2022-03-27 09:36:31 -0500

    component refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,11 @@
-import Navbar from "./Navbar";
-import Header from "./Header";
-import About from "./About";
-import Drink from "./Drink";
-import Appitizer from "./Appitizer";
-import Entree from "./Entree";
-import Dessert from "./Dessert";
-import Footer from "./Footer";
+import Menu from "./LittleOsaka/Menu";
+
+
 
 function App() {
   return (
     <div className="text-white">
-      <Navbar  transparent/> 
-      <Header />
-      <About />
-      <Drink />
-      <Appitizer />
-      <Entree />
-      <Dessert />
-      <Footer />
-
+     <Menu />
     </div>
   );
 }
