e18d2fda4c71cd4d1498ba1c20f33b31d81ed2bd
Vi Vo <aaronvo118@gmail.com>
2022-03-28 18:45:46 -0500

    intro page styles

---
+++ src/components/IntroPage/FooterIntro.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function FooterIntro() {
+  return (
+    <footer class="h-full mt-10 flex text-white font-light backdrop-blur-lg bg-opacity-10">
+  <div class="max-w-screen-xl px-4 pb-5 pt-5 mx-auto sm:px-6 lg:px-8 flex justify-center align-center">
+    
+  <div className="text-center font-light">    
+          <p> © Designed and Coded By:</p>
+          <a href="https://www.linkedin.com/in/v12v0/" className="hover:text-yellow-300"> Vi V. | </a>
+          <a href="https://www.linkedin.com/in/justin-swift-4124b4166/" className="hover:text-yellow-300" > Justin S. | </a>
+          <a href="https://www.linkedin.com/in/elizabethporporato/" className="hover:text-yellow-300"> Elizibeth P. | </a>
+          <a href="https://www.linkedin.com/in/eve-guerrero-592718200/" className="hover:text-yellow-300"> Eve G.</a> 
+      </div>
+  </div>
+</footer>
+  )
+}
+
+export default FooterIntro
--- src/components/IntroPage/Home.js
+++ src/components/IntroPage/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { NavLink, useHistory } from "react-router-dom";
+import FooterIntro from './FooterIntro';
+import AboutIntro from './AboutIntro';
 
 
 function Home() {
   return (
-    <div className="text-red-500">
-     <NavLink to="/">Home</NavLink> 
-    <NavLink to="/Menu"> Little Osaka</NavLink> 
+    <div className=" flex flex-col justify-between h-full text-white  bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+
+      <AboutIntro />
+
+      <FooterIntro />
     </div>
-   
+
   )
 }
 
