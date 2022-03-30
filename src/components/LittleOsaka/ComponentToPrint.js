78fe9907dbef02f6cb6ffc489d96e8a2572a6944
Elizabeth <elizabeth.por@gmail.com>
2022-03-29 21:43:09 -0500

    it finally started to work, will continue tomorrow

--- src/components/LittleOsaka/ComponentToPrint.js
+++ src/components/LittleOsaka/ComponentToPrint.js
@@ -1,21 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Drink from './Drink';
 import Appetizer from './Appetizer';
 import Entree from './Entree';
 import Dessert from './Dessert';
 
-class ComponentToPrint extends React.Component {
-    render() {
-        return (
-        <div style={{ display: "none" }}>
-            <Drink />
+
+const ComponentToPrint = React.forwardRef((props, ref) => { 
+    const [drinks, setDrinks] = useState([])
+    const [issueRequest] = useState(false)
+
+
+    function loadsDrinks(){
+        fetch("http://localhost:4008/categories/1")
+        .then(r => r.json())
+        .then(data => {
+            setDrinks(data.foods)
+        })
+    }
+
+    useEffect(() => {
+        loadsDrinks()
+    }, [issueRequest])
+
+    return (
+        <div ref={ref}>
+            <div className="p-5 text-2xl text-black text-[64px] text-center font-light">
+                Little
+                <span className="text-red-500"> Osaka</span>
+            </div>
+            <div className="p-5 text-2xl text-black text-[24px] text-center font-light">
+                Drinks
+            </div>
+            <ul>
+                {drinks.map((drink) => (
+                    <li key={drink.id}>{drink.name}</li>
+                ))}
+            </ul>
+            {/* <Drink />
             <Appetizer />
             <Entree />
-            <Dessert />
+            <Dessert /> */}
         </div>
+    );
+})
+
+// class ComponentToPrint extends React.Component {
+
+//     state = {
+//         drinks: []
+//     }
+
+//     componentDidMount() {
+//         fetch('http://localhost:4008/categories/1')
+//         .then((response) => response.json())
+//         .then(drinksList => {
+//             this.setState({ drinks: drinksList });
+//         });
+//     }
+
+//     render() {
+//         return (
+//         <div>
+//             <div className="p-5 text-2xl text-black text-[64px] text-center font-light">
+//                 Little
+//                 <span className="text-red-500"> Osaka</span>
+//             </div>
+//             <ul>
+//                 {this.state.drinks.map((drink) => (
+//                     <li key={drink.id}>{drink.name}</li>
+//                 ))}
+//             </ul>
+            {/* <Drink />
+            <Appetizer />
+            <Entree />
+            <Dessert /> */}
+        {/* </div>
         );
     }
-}
+} */}
 
 export default ComponentToPrint
 
