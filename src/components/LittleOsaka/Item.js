9a40ed73ef62f1a0ef59625e26fa62d6ed2a595e
Justin Swift <jlswift2@gmail.com>
2022-03-30 13:59:14 -0500

    Add link to each food item

--- src/components/LittleOsaka/Item.js
+++ src/components/LittleOsaka/Item.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Item({food}){
     return (
-    <div>
-<li>
-    <h3>{food.name}</h3>
-    <p>{food.description}</p>
-    <p>{`$ ${food.price}`}</p>
-</li>
-    </div>
-    )}
+        <Link to={`/FoodForm/${food.id}/Edit`}>
+            <div>
+                <li>
+                    <h3>{food.name}</h3>
+                    <p>{food.description}</p>
+                    <p>{`$ ${food.price}`}</p>
+                </li>
+            </div>
+        </Link>
+    )
+}
 
 export default Item;
